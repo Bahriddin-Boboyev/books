85299cefedd85db516f68dc0ee4f2dfa211d6c00
Bahriddin <bahriddinboboyev997@gmail.com>
2024-05-06 18:33:25 +0500

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Navbar } from '@/components';
 import { Button, Container } from '@mui/material';
 
 export default function Home() {
     return (
         <main>
+            <Navbar />
             <Container></Container>
         </main>
     );
